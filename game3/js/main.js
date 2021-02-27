4efa084b6638018957912c5472fe9d29594f7646
Peter <peterlim28@msn.com>
2021-02-26 20:43:12 -0500

    Added bird collecting coins functionality

--- game3/js/main.js
+++ game3/js/main.js
@@ -92,6 +92,7 @@ function create ()
 
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
     this.physics.add.overlap(player, stars, collectStar, null, this);
+    this.physics.add.overlap(bombs,stars,collectBirdStar,null,this)
 
 
     this.physics.add.collider(player, bombs, hitBomb, null, this);
@@ -135,6 +136,34 @@ function collectStar (player, star)
 
     //  Add and update the score
     score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        //  A new batch of stars to collect
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+
+    }
+}
+
+function collectBirdStar (player, star)
+{
+    star.disableBody(true, true);
+
+    //  Add and update the score
+    score -= 10;
     scoreText.setText('Score: ' + score);
 
     if (stars.countActive(true) === 0)
